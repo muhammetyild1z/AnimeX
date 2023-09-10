273fc3204311b03351b2d863a206a7d735c7f5c8
muhammetyild1z <muhammety.yildizz@gmail.com>
2023-09-10 21:56:24 +0300

    .

---
+++ AnimeX/AnimeX/ViewComponents/Footer/_Footer.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnimeX.ViewComponents.Footer
+{
+    public class _Footer:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
