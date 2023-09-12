7b7d90661d0112673c3e2356ff970b6c6f35000f
muhammetyild1z <muhammety.yildizz@gmail.com>
2023-09-12 18:38:20 +0300

    .

---
+++ AnimeX/DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using AnimeX.DataAccessLayer.Abstract;
+using AnimeX.DataAccessLayer.Concrate;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeX.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly Context _context;
+
+        public GenericRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public async Task<T> GetByIDAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<List<T>> GetListAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public void Insert(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity, T unchanged)
+        {
+            _context.Entry(unchanged).CurrentValues.SetValues(entity);
+        }
+    }
+}
