22198925ce3398cf30a3710738498e2fc1095e7d
muhammetyild1z <muhammety.yildizz@gmail.com>
2023-09-15 17:23:56 +0300

    .

---
+++ AnimeX/AnimeX/Controllers/AnimeIzleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnimeX.UI.Controllers
+{
+    public class AnimeIzleController : Controller
+    {
+        public IActionResult Izle()
+        {
+            return View();
+        }
+    }
+}
