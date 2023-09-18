3f57519376d79cdd1ae052e9ac074273b5ffc64a
muhammetyild1z <muhammety.yildizz@gmail.com>
2023-09-18 18:09:13 +0300

    .

--- AnimeX/AnimeX/Controllers/AnimelerController.cs
+++ AnimeX/AnimeX/Controllers/AnimelerController.cs
@@ -1,6 +1,7 @@
 ﻿using AnimeX.BusinnessLayer.Concrate;
 using AnimeX.DataAccessLayer.Concrate;
 using AnimeX.DataAccessLayer.EntityFramework;
+using EntityLayer;
 using Microsoft.AspNetCore.Mvc;
 using X.PagedList;
 
@@ -9,11 +10,19 @@ namespace AnimeX.UI.Controllers
     public class AnimelerController : Controller
     {
         AnimelerManager am = new AnimelerManager(new efAnimelerRepository(new Context()));
+        CategoriesManager amn = new CategoriesManager(new efCatagoriesRepository(new Context()));
+        [HttpGet]
         public  IActionResult Index(int page = 1)
-        {          
+        {
+            ViewBag.category = amn.TGetList().Select(x=>x.KategoriAdi);
             return View( am.TGetList().ToPagedList(page, 18));
+            
         }
-        
+        [HttpPost]
+        public IActionResult Index()
+        {
+            return View();
+        }
 
 
     }
