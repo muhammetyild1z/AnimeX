64e39223d5f4ad5c8f85a83e81e94a73dd87a44d
muhammetyild1z <muhammety.yildizz@gmail.com>
2023-10-25 19:05:43 +0300

    .

--- AnimeX/AnimeX/Program.cs
+++ AnimeX/AnimeX/Program.cs
@@ -83,8 +83,17 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
 
 app.Run();
